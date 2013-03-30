11c3ee51872461e0e19649aa828e0b170b53ffac
jcli <jcjc.li@gmail.com>
2013-03-30 14:40:24 -0700

    update

--- bookmark/views.py
+++ bookmark/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import Context, loader
 
 def index(request):
-    template = loader.get_template('bookmark/index.djhtml')
+    template = loader.get_template('bookmark/signin.html')
     context = Context({
             'latest_poll_list': "lkajdsf",
             })

30562c0c47f74cb6b46d2cd08bcb133aee86cc55
jcli <jcjc.li@gmail.com>
2013-03-30 16:53:07 -0700

    updates

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from bookmark import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index')
+                       url(r'^$', views.index, name='index'),
+                       url(r'^signup/$', views.user_signup, name='user_signup'),
+                       url(r'^user/$', views.user_view, name='user'),
                    )
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
+from django import forms
 
 def index(request):
     context = Context({
         'signin':'Bookmark Manager'
     })
-    return render_to_response('bookmark/signin.djhtml', context)
+    return render_to_response('bookmark/signin.djhtml', context, context_instance=RequestContext(request))
+
+def user_signup(request):
+    context = Context({
+        'signup':'Bookmark Manager'
+    })
+    return render_to_response('bookmark/signup.djhtml', context, context_instance=RequestContext(request))
+    
+def user_view(request):
+    user_email = request.POST['user_email']
+    password = request.POST['password']
+
+    print (user_email)
+    print (password)
+
+    return HttpResponse("thank you")
+
+def user_signup_response(request):
+
+    return HttpResponse("thank you for signing up")
